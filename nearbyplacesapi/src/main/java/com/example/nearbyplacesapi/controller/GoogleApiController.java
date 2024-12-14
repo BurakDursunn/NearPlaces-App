b52b10bf28137bb26a47563456e5a1183fcecf79
Burak Dursun <burakdursunn.bd@gmail.com>
2024-12-14 21:19:01 +0300

    Config class is created for solving error on GoogleApiController.java

--- nearbyplacesapi/src/main/java/com/example/nearbyplacesapi/controller/GoogleApiController.java
+++ nearbyplacesapi/src/main/java/com/example/nearbyplacesapi/controller/GoogleApiController.java
@@ -1,19 +1,24 @@
 package com.example.nearbyplacesapi.controller;
 
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.ResponseEntity;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class GoogleApiController {
 
-    @Value("${google.maps.api.key}")
-    private String googleApiKey;
+    private final String googleApiKey;
+
+    @Autowired
+    public GoogleApiController(String googleApiKey) {
+        this.googleApiKey = googleApiKey;
+    }
 
     @GetMapping("/api/google-api-key")
-    public ResponseEntity<String> getGoogleApiKey() {
-        return ResponseEntity.ok(googleApiKey);
+    public String getGoogleApiKey() {
+        return googleApiKey;
     }
 }
 
